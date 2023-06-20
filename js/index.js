d772544622830d981ab9979a9545401267a1d102
Mykola <kolanuch13@gmail.com>
2023-06-20 23:55:58 +0300

    New fonts, remove some features.

--- js/index.js
+++ js/index.js
@@ -8,22 +8,23 @@ const galleryPlace = document.querySelector(".gallery");
           <li class="gallery__item">
             <figure>
               <a class="gallery__link" href="${item.original}">
-                <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+                <img class="gallery__image" src="${item.preview}" title="${item.description}"/>
               </a>
-              <figcapture class="gallery__item--description">${item.description}</figcapture>
             </figure>
           </li>
-          `;
-    })
+              `;
+            })
     .join("");
 
 
-galleryPlace.insertAdjacentHTML("beforeend", markup);
-
-let lightbox = new SimpleLightbox(".gallery a", {
-  fadeSpeed: 250,
-  doubleTapZoom: 2,
-  showCounter: false,
-});
-
+    galleryPlace.insertAdjacentHTML("beforeend", markup);
+    
+    let lightbox = new SimpleLightbox(".gallery a", {
+      fadeSpeed: 250,
+      doubleTapZoom: 2,
+      showCounter: false,
+      captions: true,
+    });
+    
+    // <figcapture class="gallery__item--description">${item.description}</figcapture>
 
